875f6adb90dec2b2bf4150f4640b90844df57ea2
Andrew Carvajal <acarvajal@tendigi.com>
2018-09-10 15:58:58 -0400

    Connected buttons to their view controller

---
+++ tictactoe/tictactoe/PlayViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PlayViewController.swift
+//  tictactoe
+//
+//  Created by Andrew Carvajal on 9/10/18.
+//  Copyright © 2018 Tendigi. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func firstButtonTapped(_ sender: UIButton) {
+        print("1")
+    }
+    
+    @IBAction func secondButtonTapped(_ sender: UIButton) {
+        print("2")
+    }
+    
+    @IBAction func thirdButtonTapped(_ sender: UIButton) {
+        print("3")
+    }
+    
+    @IBAction func fourthButtonTapped(_ sender: UIButton) {
+        print("4")
+    }
+    
+    @IBAction func fifthButtonTapped(_ sender: UIButton) {
+        print("5")
+    }
+    
+    @IBAction func sixthButtonTapped(_ sender: UIButton) {
+        print("6")
+    }
+    
+    @IBAction func seventhButtonTapped(_ sender: UIButton) {
+        print("7")
+    }
+    
+    @IBAction func eighthButtonTapped(_ sender: UIButton) {
+        print("8")
+    }
+    
+    @IBAction func ninthButtonTapped(_ sender: UIButton) {
+        print("9")
+    }
+}
+
